66f1e08700511062408124e4dd2f02a8e6978e7f
Naiel <109038805+naielv@users.noreply.github.com>
2024-08-05 14:53:01 +0200

    Update main.py

--- worker/micropython/main.py
+++ worker/micropython/main.py
@@ -1,11 +1,13 @@
 import machine
 import time
 import ubinascii
+import json
 
 from umqtt.simple import MQTTClient
 
 # These defaults are overwritten with the contents of /config.json by load_config()
 CONFIG = {
+    "node": "ES48920-MP-001"
     "broker": "",
     "user": "",
     "passw": "",
@@ -13,15 +15,30 @@ CONFIG = {
     "client_id": b"micropython_" + ubinascii.hexlify(machine.unique_id()),
     "topic": b"home",
 }
+def safe_serialize(obj):
+  default = lambda o: f"<non-serializable: {type(o).__qualname__}>"
+  return json.dumps(obj, default=default)
 
 client = None
 def sub_cb(topic, msg):
-    print((topic, msg))
-  
+    print(msg)
+    print(topic)
+    ms = json.loads(msg.decode("utf-8"))
+    if ms["type"] == "python-inline":
+        loc = {}
+        try:
+          exec(ms["script"], globals(), loc)
+          client.publish(ms["out-topic"], safe_serialize(loc))
+        except Exception as e:
+          client.publish(ms["out-topic"], safe_serialize({"_err": str(e)}))
+    elif ms["type"] == "cmd":
+        client.publish(ms["out-topic"], safe_serialize({"_err": str("This is a MicroPython node, the cmd type is not compatible.")}))
+    print(msg.topic+" "+str(ms))
 def main():
     client = MQTTClient(CONFIG['client_id'], CONFIG['broker'], CONFIG["port"], CONFIG["user"], CONFIG["passw"], 0, CONFIG["ssl"])
     client.set_callback(sub_cb)
     client.connect()
+    client.subscribe(f"axlcore/node/{CONFIG['node']}".encode("utf-8"))
     print("Connected to {}".format(CONFIG['broker']))
     while True:
         if True:
