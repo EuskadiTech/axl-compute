a38e1d58e403e1454ff34b29c0105fcf032b4062
Naiel <109038805+naielv@users.noreply.github.com>
2024-08-13 19:49:57 +0200

    Update main.py

--- worker/micropython/main.py
+++ worker/micropython/main.py
@@ -6,26 +6,41 @@ import network
 import socket
 from time import sleep
 from umqtt.simple import MQTTClient
-    
+import gc
+import ssl
+from machine import Pin
+
+led = Pin("LED", Pin.OUT)
+led.on()
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.verify_mode = ssl.CERT_NONE
 # These defaults are overwritten with the contents of /config.json by load_config()
 CONFIG = {
     "node": "ES48920-MP-001",
-    "ap_name": "Gameme 2.4G",
-    "ap_pass": "D9RFYKCA76PFYXH4",
+    "ap_name": "",
+    "ap_pass": "",
     "broker": "",
-    "port": 0,
+    "port": 8883,
     "user": "",
     "passw": "",
-    "ssl": True,
-    "client_id": b"micropython_" + ubinascii.hexlify(machine.unique_id()),
-    "topic": b"home",
+    "ssl": context,
+    "client_id": b"micropython_" + ubinascii.hexlify(machine.unique_id())
 }
 def safe_serialize(obj):
-  default = lambda o: f"<non-serializable: {type(o).__qualname__}>"
-  return json.dumps(obj, default=default)
+    out = {
+        "out": obj.get("out"),
+        "_err": obj.get("out"),
+        "out1": obj.get("out1"),
+        "out2": obj.get("out2"),
+        "out3": obj.get("out3"),
+        "out4": obj.get("out4"),
+    }
+    return json.dumps(obj)
 
 client = None
 def sub_cb(topic, msg):
+    gc.collect()
     print(msg)
     print(topic)
     ms = json.loads(msg.decode("utf-8"))
@@ -38,8 +53,9 @@ def sub_cb(topic, msg):
           client.publish(ms["out-topic"], safe_serialize({"_err": str(e)}))
     elif ms["type"] == "cmd":
         client.publish(ms["out-topic"], safe_serialize({"_err": str("This is a MicroPython node, the cmd type is not compatible.")}))
-    print(msg.topic+" "+str(ms))
-    
+    print(str(topic)+" "+str(ms))
+    gc.collect()
+    led.toggle()
 def connect():
     #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
@@ -50,7 +66,8 @@ def connect():
         sleep(1)
     print(wlan.ifconfig())
 def main():
-    client = MQTTClient(CONFIG['client_id'], CONFIG['broker'], CONFIG["port"], CONFIG["user"], CONFIG["passw"], 0, CONFIG["ssl"])
+    global client
+    client = MQTTClient(client_id=CONFIG['client_id'], server=CONFIG['broker'], port=CONFIG["port"], user=CONFIG["user"], password=CONFIG["passw"], ssl=CONFIG["ssl"])
     client.set_callback(sub_cb)
     client.connect()
     client.subscribe(f"axlcore/node/{CONFIG['node']}".encode("utf-8"))
@@ -58,12 +75,13 @@ def main():
     while True:
         if True:
             # Blocking wait for message
-            c.wait_msg()
+            client.wait_msg()
         else:
             # Non-blocking wait for message
-            c.check_msg()
+            client.check_msg()
             # Then need to sleep to avoid 100% CPU usage (in a real
             # app other useful actions would be performed instead)
+            gc.collect()
             time.sleep(1)
 
 if __name__ == '__main__':
